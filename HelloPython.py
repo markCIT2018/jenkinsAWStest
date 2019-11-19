7a16dec05946e8c352cc498ee5653420ae3e729f
markCIT2018 <mark.p.crowley@mycit.ie>
2019-11-19 11:57:03 +0000

    fuck

--- HelloPython.py
+++ HelloPython.py
@@ -1,4 +1,4 @@
-kjwehfownehwjufejbhjkiogjkegieojghkjkbweifhbhjklwjefoijfnjwifowjjwgqgelnjnsome 
+fuckthepolicekjwehfownehwjufejbhjkiogjkegieojghkjkbweifhbhjklwjefoijfnjwifowjjwgqgelnjnsome 
 nonabghuigbhugdfsff
 import datetime
 print("Hello world!")
